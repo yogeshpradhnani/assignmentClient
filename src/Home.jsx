4fbb58cd6e127db91ba0519ff2f5fbc586f0c6a5
yogeshpradhnani <yogeshpradhnani99@gmail.com>
2025-03-01 23:42:11 +0530

    done

--- src/Home.jsx
+++ src/Home.jsx
@@ -81,7 +81,7 @@ export default function Home() {
           >
             {showFilter ? "Close Filters" : "Show Filters"}
           </button>
-          <button className="bg-green-600 text-white px-4 py-2 rounded-md w-full" onClick={() => navigate('/customerBookings')} >
+          <button className="bg-green-600 text-white px-4 py-2 rounded-md w-full" onClick={() => navigate('/bookings')} >
             Show Bookings
           </button>
         </div>
